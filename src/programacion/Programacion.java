78374358d48ee6dc0c58174690b8d323780f70ea
GastonLell <gastonlell.freelance@gmail.com>
2025-08-19 22:29:54 -0300

    Trabajo Practico Programacion estructurada

---
+++ src/programacion/Programacion.java
@@ -0,0 +1,13 @@
+package programacion;
+
+/**
+ *
+ * @author gastonlell
+ */
+public class Programacion {
+
+    public static void main(String[] args) {
+        Programacion_estructurada Trabajo_uno = new Programacion_estructurada();
+    }
+    
+}
